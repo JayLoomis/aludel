411cf1182b6c44c77d67e4e0e086597025bd4e7f
Jay Loomis <jay.loomis@gmail.com>
2016-02-24 14:44:49 -0800

    More noodling

--- aludel.py
+++ aludel.py
@@ -1,26 +1,11 @@
 # I'm going to start by making a basic D&D 5th edition character generator,
 # which I'll then expand into fully-fledged classes and whatnot.
 
-stats_def = (('str', 'strength'), ('dex', 'dexterity'), ('con', 'constitution'),
-             ('int', 'intelligence'), ('wis', 'wisdom'), ('cha', 'charisma'))
-
-my_stats = {}
-
-for stat_def in stats_def:
-    my_stats[stat_def[0]] = {'name': stat_def[1], 'val': 10, 'mod': 0}
-
-print(my_stats['str']['name'])
-
-
-# def print_stats(stats):
-#     for stat in stats:
-#         print('{} - {} ({})'.format(stat['name'], stat['val'], stat['mod']))
-#
-# print_stats(my_stats)
 
 standard_ability_list = [('strength', 'str'), ('dexterity', 'dex'),
                          ('constitution', 'con'), ('intelligence', 'int'),
                          ('wisdom', 'wis'), ('charisma', 'cha')]
+
 
 class Ability:
     def __init__(self, ability_name, short_name):
@@ -30,12 +15,23 @@ class Ability:
         self.__mods = []
 
     def get_regular_value(self):
-        pass
+        reg_val = self.__base
+
+        for mod in self.__mods:
+            if mod['type'] != 'conditional':
+                reg_val += mod['value']
+
+        return reg_val
 
     def get_conditional_values(self):
         pass
 
     def has_conditional_mods(self):
+        for mod in self.__mods:
+            if mod['type'] == 'conditional':
+                return True
+
+        return False
 
     def get_ability_mod(self):
         pass
@@ -45,6 +41,7 @@ class Ability:
 
     def add_modifier(self, value, name, source):
         self.__mods.append({'value': value, 'name': name, 'source': source})
+
 
 class Abilities:
     def __init__(self, ability_list):
